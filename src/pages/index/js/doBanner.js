883d6ba422a79419454396f080931cbf9f80994e
White Egg <2733259150@qq.com>
2023-10-04 17:42:56 +0800

    这个文件没用了

--- src/pages/index/js/doBanner.js
+++ src/pages/index/js/doBanner.js
@@ -1,12 +1,7 @@
 import { banner } from "../../../network/banner";
 
 export default function () {
-    // 请求广告
-    banner().then((res) => {
-        const swiper = res.data.result
-        return {
-            swiper
-        }
-    })
+    
+ 
 
 }
