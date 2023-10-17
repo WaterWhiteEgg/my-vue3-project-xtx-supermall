824e21f20396308efa4cf374735230369c1be7ba
White Egg <2733259150@qq.com>
2023-10-17 17:40:11 +0800

    加热门 推荐点进去的路由

---
+++ src/pages/hot/hot.vue
@@ -0,0 +1,14 @@
+<template>
+    <view class="hot"></view>
+    title
+</template>
+
+<script>
+</script>
+
+<style>
+.hot {
+    height: 100vh;
+    background-color: #000000;
+}
+</style>
--- src/pages/index/indexChild/indexHotMutil.vue
+++ src/pages/index/indexChild/indexHotMutil.vue
@@ -1,14 +1,20 @@
 <template>
     <view class="hotmutil">
-        <view v-for="(item, index) in hotMutilData" :key="item.id" class="hotmutil-item">
-            <view class="hotmutil-textbox">
-                <text>{{ item.title }}</text>
-                <text class="hotmutil-alt">{{ item.alt }}</text>
-            </view>
-            <view class="hotmutil-imagebox">
-                <image :src="item.pictures[0]" mode="scaleToFill" />
-                <image :src="item.pictures[1]" mode="scaleToFill" />
-            </view>
+        <view
+            v-for="(item, index) in hotMutilData"
+            :key="item.id"
+            class="hotmutil-item"
+        >
+            <navigator url="/pages/hot/hot" hover-class="none" class="cards">
+                <view class="hotmutil-textbox">
+                    <text>{{ item.title }}</text>
+                    <text class="hotmutil-alt">{{ item.alt }}</text>
+                </view>
+                <view class="hotmutil-imagebox">
+                    <image :src="item.pictures[0]" mode="scaleToFill" />
+                    <image :src="item.pictures[1]" mode="scaleToFill" />
+                </view>
+            </navigator>
         </view>
     </view>
 </template>
@@ -17,10 +23,10 @@ const props = defineProps({
     hotMutilData: {
         type: Array,
         default: function () {
-            return []
-        }
-    }
-})
+            return [];
+        },
+    },
+});
 </script>
 
 <style>
@@ -44,8 +50,6 @@ const props = defineProps({
     display: flex;
     flex-direction: column;
     margin: 1vh 4vw 0 4vw;
-
-
 }
 
 .hotmutil-imagebox image {
@@ -53,4 +57,5 @@ const props = defineProps({
     padding: 1vh 1vw;
     width: 17vw;
     height: 10vh;
-}</style>
+}
+</style>
