741a372e1fe54dbb1d40ae33714d3a75c3e3722f
White Egg <2733259150@qq.com>
2023-10-26 14:56:03 +0800

    展示所有列表页的制作

---
+++ src/pages/showall/showall.vue
@@ -0,0 +1,18 @@
+<template>
+  <show-static-product :dataArray="goodsData"></show-static-product>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { onLoad } from "@dcloudio/uni-app";
+
+import showStaticProduct from "../../components/showStaticProduct/showStaticProduct.vue";
+// 获取query并提供数据
+const goodsData = ref([]);
+onLoad((query) => {
+  goodsData.value = JSON.parse(query.goodsData);
+});
+</script>
+
+<style >
+</style>
