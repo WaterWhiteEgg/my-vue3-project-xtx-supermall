54daa14902b10592f4505e8c1b3a30a096f3ea7f
White Egg <2733259150@qq.com>
2023-11-13 17:35:32 +0800

    user的头部样式制作

---
+++ src/pages/user/child/userMethod.vue
@@ -0,0 +1,18 @@
+<template>
+<view class="method">方法</view>  
+</template>
+<script setup>
+const props = defineProps({
+
+})
+</script>
+<style scoped>
+.method{
+    width: 96vw;
+    height: 10vh;
+    margin: auto  ;
+    margin-top: -7vh;
+
+    background-color: red;
+}
+</style>
--- src/pages/user/user.vue
+++ src/pages/user/user.vue
@@ -1,11 +1,31 @@
 <template>
-    <view>欢迎用户： {{ datas.account }} ！</view>
+        <view class="information-bar" :style="{ 'padding-top': safeAreaInsets.top + 'px' }">
+            <view class="information">
+                <view class="information-avatar">
+                    <image :src="datas.avatar" mode="scaleToFill" />
+                </view>
+                <view class="information-account">
+                    <text class="information-account-username">dddddddddddddddddddddd无登录{{ datas.account }}</text>
+                    <text class="information-account-number">点击注册账号</text>
+                </view>
+                <view class="information-system">
+                    <text>设置</text>
+                </view>
+            </view>
+        </view>
+
+    <userMethod></userMethod>
 </template>
 
 <script setup>
 import { onLoad, onShow } from '@dcloudio/uni-app';
 import { useRequest } from "../../store/modules/request"
 import { ref, watch } from 'vue'
+
+import userMethod from "./child/userMethod.vue"
+
+// 通过安全距离的获取可以动态的适配各种机型的头部距离
+const { safeAreaInsets } = uni.getSystemInfoSync();
 
 const datas = ref({})
 onLoad((query) => {
@@ -33,4 +53,64 @@ const testToken = () => {
 }
 </script>
 
-<style></style>
+<style scoped>
+.information-box {}
+
+.information-bar {
+    padding-bottom: 10vh;
+    font-size: 30rpx;
+    border-bottom-left-radius: 5%;
+    border-bottom-right-radius: 5%;
+    background-color: #00c2a5;
+
+    color: #fff;
+}
+
+.information {
+    display: flex;
+
+}
+
+.information-avatar {
+    margin: 0 2vw;
+}
+
+.information-avatar image {
+    width: 14vw;
+    height: 9.5vh;
+    border-radius: 50%;
+
+}
+
+.information-account {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding-top: 1vh;
+    font-size: 30rpx;
+}
+
+.information-account-number {
+    font-size: 25rpx;
+}
+
+.information-account-username {
+    width: 100%;
+    max-width: 60vw;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+
+.information-system {
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+    width: 30%;
+
+}
+
+.information-system text {
+    padding: 0 3vw 2vh 0;
+}
+</style>
