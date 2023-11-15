e6c2c05763e9fba6118284fb80d141209e325e63
White Egg <2733259150@qq.com>
2023-11-15 17:36:57 +0800

    样式制作

--- src/pages/user/child/userMethod.vue
+++ src/pages/user/child/userMethod.vue
@@ -1,11 +1,15 @@
 <template>
     <view class="method">
-        <view class="my-order">我的订单</view>
+        <view class="method-title">
+            <view>我的订单</view>
+            <view class="method-title-button" >查看全部订单></view>
+        </view>
         <view class="method-bar">
             <view class="method-bar-item" v-for="(item, index) in userMethods" :key="item.id">
                 <image :src="item.ico" mode="scaleToFill" />
                 <text class="method-bar-item-name">{{ item.name }}</text>
             </view>
+
         </view>
 
     </view>
@@ -24,15 +28,24 @@ const userMethods = ref([
     { name: "待评价", ico: "../../../static/color/daipingjia.png", id: "beEvaluated" },
     { name: "售后", ico: "../../../static/color/shouhou.png", id: "afterSales" },
 ])
+
+// 前往设置
+const toSettings = () => {
+    uni.navigateTo({
+        url: "/pageMember/settings/settings"
+    })
+}
 </script>
 <style scoped>
 .method {
     width: 96vw;
-    height: 10vh;
     margin: auto;
     margin-top: -7vh;
-    font-size: 35rpx;
+    margin-bottom: 5vh;
+    border-radius: 5rpx;
+    box-shadow: 0rpx 0rpx 3rpx #000;
     background-color: #fff;
+
 }
 
 .method-bar {
@@ -55,7 +68,21 @@ const userMethods = ref([
     object-fit: cover;
     /* 保持宽高比并填充整个容器，可能会裁剪部分内容 */
 }
-.method-bar-item-name{
+
+.method-bar-item-name {
     padding: 1vh 0;
 }
+
+.method-title {
+    display: flex;
+    justify-content: space-between;
+    padding: 2vh 4vw;
+    font-size: 30rpx;
+}
+
+.method-title-button {
+    font-size: 25rpx;
+    color: #8d8d8d;
+}
 </style>
+
