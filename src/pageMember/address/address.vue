ae448baa9a0fd87d3807e1f61b780814d778b21a
White Egg <2733259150@qq.com>
2023-11-28 19:44:46 +0800

    基础样式

--- src/pageMember/address/address.vue
+++ src/pageMember/address/address.vue
@@ -1,39 +1,45 @@
 <template>
     <view>
-        <scroll-view scroll-x v-for="item in data" class="scroll">
-            <view class="item">
-                <view class="item-detail">
-                    <view class="item-detail-name">
-                        <view>{{ item.name }} {{ item.number }}</view>
-                        <text>选择</text>
-                    </view>
-                    <view class="item-detail-address">{{ item.address }}</view>
+        <view class="item" v-for="item in data" :key="item.address">
+            <view class="item-detail">
+                <view class="item-detail-name">
+                    <view>{{ item.name }} {{ item.number }}</view>
+                    <text>选择</text>
                 </view>
-                <view class="item-fn">
-                    <view class="modify">修改</view>
-                    <view class="del">删除</view>
-                </view>
+                <view class="item-detail-address">{{ item.address }}</view>
             </view>
-        </scroll-view>
+            <view class="item-fn">
+                <uni-swipe-action>
+                    <uni-swipe-action-item>
+                        <view class="modify">修改</view>
+                        <template #right>
+                            <view class="del">删除</view>
+                        </template>
+
+                    </uni-swipe-action-item>
+                </uni-swipe-action>
+            </view>
+        </view>
     </view>
 </template>
 <script setup>
+import { ref } from 'vue';
+import { debounce } from "../../utils/debounce"
+
 // 测试用的数据
 const data = [{ name: 'zc', number: '11111111111', address: '存储池' }]
+
+
+
 </script>
 <style scoped>
-.scroll {
-    width: 90vw;
-    max-height: 200rpx;
-    margin: 0 auto;
-
-}
-
 .item {
     display: flex;
     justify-content: space-between;
     /* 根据删除盒子的width加上原本长度出现的拖动效果 */
-    width: 103vw;
+    width: 90vw;
+    margin: 0 auto;
+
     height: 13vh;
     font-size: 35rpx;
     border-bottom: 1px solid #6161612d;
@@ -52,14 +58,13 @@ const data = [{ name: 'zc', number: '11111111111', address: '存储池' }]
 
 .item-fn {
     display: flex;
-    height: 100%;
+    
 }
 
 .del {
     display: flex;
     align-items: center;
     justify-content: center;
-    height: 100%;
     width: 13vw;
     background-color: #e91a1a;
     color: #fff;
@@ -70,6 +75,8 @@ const data = [{ name: 'zc', number: '11111111111', address: '存储池' }]
     position: relative;
     padding: 0 3vw;
     padding-top: 2vh;
+    height: 11vh;
+
     font-size: 27rpx;
     color: #797979;
 }
--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -1,6 +1,5 @@
 export const debounce = (func, delay = 300) => {
     let timerId;
-
     return (...args) => {
         clearTimeout(timerId);
         timerId = setTimeout(() => {
