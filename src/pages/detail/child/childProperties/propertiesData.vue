679da938ab469126542ef0eec909d3a35c4832ce
White Egg <2733259150@qq.com>
2023-12-05 14:31:25 +0800

    skus值的计算

--- src/pages/detail/child/childProperties/propertiesData.vue
+++ src/pages/detail/child/childProperties/propertiesData.vue
@@ -6,7 +6,7 @@
       <view class="show-desc-price"><text class="show-desc-price-ico">￥</text>
         {{ detailSkus[0].price }}</view>
       <view class="show-desc-inventory">库存：{{ detailSkus[0].inventory }}</view>
-      <view class="show-desc-select">已选：</view>
+      <view class="show-desc-select">已选：{{ skusArray }}</view>
     </view>
   </view>
   <scroll-view scroll-y class="scroll">
@@ -59,11 +59,15 @@ const emits = defineEmits(["itemClick"]);
 
 // sku对应的数组
 const skusArray = ref([])
+// sku的item对象
+const skusItem = ref({})
 // 初始化skusArray的初始值
 onMounted(() => {
   // 使skusArray初始化第一个值
-  skusArray.value = props.detailSkus[0].specs
-
+  props.detailSpecs.forEach((item) => {
+    skusArray.value.push(item.values[0])
+  })
+  // console.log(skusArray.value);
 })
 
 
@@ -73,12 +77,22 @@ const activeItem = ref([]);
 // 通过数组的index决定 detailSpecs 的循环里的 item.values
 // 再决定谁是活跃点击
 const changeActiveItem = (index, itemIndex, itemValue, name) => {
+  // 将对应活跃的index切换
   activeItem.value[index] = itemIndex;
-  // 切换完活跃点击后决定skus里面的哪个对象
+  // 切换skusArray对应对象的数据
+  skusArray.value[index] = props.detailSpecs[index].values[itemIndex]
+  // 切换完后决定skus里面的哪个对象
   selectDetailSkus(itemValue, name)
 
 };
+// 样式切换的布尔值函数
 const isActiveItem = (index, itemIndex) => {
+  // 如果activeItem是空的，则初始化它都为0
+  if (!activeItem.value.length) {
+    props.detailSpecs.forEach(() => {
+      activeItem.value.push(0)
+    })
+  }
   return activeItem.value[index] === itemIndex;
 };
 // 将提供的数据渲染到父组件
@@ -91,10 +105,26 @@ const itemClick = (itemValue, index) => {
 const selectDetailSkus = (itemValue, name) => {
   // 遍历detailSkus，寻找相同型号
   for (let item of props.detailSkus) {
-    console.log(skusArray.value);
+    // 记录所有条件正确的值，判断skus的关系
+    const isAllItem = ref(true)
+    // 再次循环，将specs里面的值进行对比
+    for (let index = 0; index < item.specs.length; index++) {
+      // 对比值
+      // console.log(item.specs[index].valueName === skusArray.value[index].name);
+      // 假如这次循环有条件不过关则修改isAllItem的值
+      if (item.specs[index].valueName !== skusArray.value[index].name) {
+        isAllItem.value = false
+        break;
+      }
+    }
+    // 根据isAllItem的值判断是否全都正确
+    if (isAllItem.value) {
+      // 若全都正确则将这个sku记录
+      console.log(item);
+    }
   }
   // console.log(itemValue === );
-  console.log(props.detailSkus);
+  // console.log(props.detailSkus);
 }
 // 数量记录
 const quantity = ref(1);
